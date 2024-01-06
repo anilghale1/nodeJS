3a4cf6cfd88810e36f1aac23286776af47162b81
anilghale1 <ghalepasang5@gmail.com>
2024-01-06 23:01:56 +0545

    created server installed module packages express nodeMon and learned about require and import

---
+++ main.js
@@ -0,0 +1,11 @@
+// import {a,b,c} from "./myModule.js";
+
+// console.log(a);
+// console.log(b);
+// console.log(c);   named import
+
+// import obj from "./myModule.js";
+// console.log(obj);
+
+ const a = require("./myModule2.js");
+ console.log(a);
---
+++ myModule.js
@@ -0,0 +1,9 @@
+export const a = 1;
+export const b = 2;
+export const c = 3;
+
+let obj;
+export default obj = {
+    x : 5,
+    y : 10
+}
